5c70435363f43901f25830b4af49cb3d60e4bf3c
Anna Bottum <annabottum@Annas-MacBook-Pro.local>
2020-04-14 21:12:02 -0500

    added first bit of upload function

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,6 @@
+import csv, json, sys
+import os
+#from ocr_core import ocr_core
 from app import app,login, db
 from flask import render_template, request
 from werkzeug.urls import url_parse
@@ -6,6 +9,19 @@ from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from datetime import datetime
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+
+# define a folder to store and later serve the images
+UPLOAD_FOLDER = '/static/uploads/'
+# allow files of a specific type
+ALLOWED_EXTENSIONS = set(['csv','xls','xlsx'])
+
+# function to check the file extension
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 @app.route('/index')
@@ -27,7 +43,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('upload_file'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -35,10 +51,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
+        #next_page = request.args.get('next')
+        #if not next_page or url_parse(next_page).netloc != '':
+        #    next_page = url_for('upload_file')
+        return redirect(url_for('upload_file'))
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
@@ -74,3 +90,43 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_login = datetime.utcnow()
         db.session.commit()
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if there is a file in the request
+        if 'file' not in request.files:
+            return render_template('upload.html', msg='No file selected')
+        file = request.files['file']
+        # if no file is selected
+        if file.filename == '':
+            return render_template('upload.html', msg='No file selected')
+
+        if file and allowed_file(file.filename):
+	    if True:
+
+
+                with open(file) as csv_file:
+                    csv_reader = csv.reader(csv_file, delimiter=',')
+                    line_count = 0
+                    for row in csv_reader:
+                        if line_count == 0:
+                            headers = row
+                            line_count += 1
+                        else:
+                            line_count += 1
+
+
+	    
+
+
+ 
+		
+
+            return render_template('upload.html',
+                                   headers=headers,
+                                   img_src=UPLOAD_FOLDER + file.filename)
+    elif request.method == 'GET':
+        print('here also')
+        return render_template('upload.html')
+    return render_template('upload.html')
