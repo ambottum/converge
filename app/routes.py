60d2e5be086f25bbf93cbd4dce77fe76fc7066a6
Anna Bottum <annabottum@Annas-MacBook-Pro.local>
2020-05-17 15:26:19 -0500

    Nearly finished for final packaging. Needs final reviewing

--- app/routes.py
+++ app/routes.py
@@ -135,7 +135,7 @@ def upload_file():
                         line_count += 1
                     else:
                         line_count += 1              
-            #replace spaces with underscores
+            #replace spaces with underscores so that header names can be passed to html
             headers1_ = []  
             headers2_ = []          
             for i in range(len(headers1)):
@@ -171,6 +171,10 @@ def get_headers():
             merge_headers1 = request.form.getlist('file1')
             merge_headers2 = request.form.getlist('file2')
 
+            if 'fkey1' not in request.form.keys() or 'fkey2' not in request.form.keys():
+                flash('Please be sure to select a primary key for each file')
+                return render_template('upload.html')
+
             foreign_key1 = request.form['fkey1']
             foreign_key2 = request.form['fkey2']
 
@@ -181,19 +185,15 @@ def get_headers():
             form = HiddenFields(path1=path1, path2=path2, hmerge_headers1=mh_string1, hmerge_headers2=mh_string2, hforeign_key1=foreign_key1, hforeign_key2=foreign_key2)
           
             
-            print("FIRST PASS merge_headers1 ",merge_headers1)
             #return render_template('ignore_fields.html', merge_headers1=merge_headers1, merge_headers2=merge_headers2, filename1=filename1, filename2=filename2, form=form)
 
-            print("JUST BEFORE NOZERO IF", form)
             if no_zero and no_zero[0] == 'on':
-                print("NO_ZERO ",no_zero)
                 return render_template('ignore_fields.html', merge_headers1=merge_headers1, merge_headers2=merge_headers2, filename1=filename1, filename2=filename2, form=form)
 
             #return render_template('merged_file.html')    
 
             #second pass will NOT have a list of merge_headers because it is coming from ignore_fields. so we need to grab the list of merge_headers from the hiddenforms
         else:
-            print("SECOND PASS ", request.values.__dict__)
             form_fields = HiddenFields(request.values)
             form_paths = HiddenPaths(request.values)
 
@@ -208,79 +208,166 @@ def get_headers():
             merge_headers2 = form_fields.data['hmerge_headers2'].split(",")
             foreign_key1 = form_fields.data['hforeign_key1']
             foreign_key2 = form_fields.data['hforeign_key2']
-            
-            no_zero = [] #make no_zero an empty list so that it doesn't enter if statement below
+
+            # print('foreign_key1 and its length', foreign_key1, len(foreign_key1))
+            # print('foreign_key2 and its length', foreign_key2, len(foreign_key2))
+            # if not foreign_key1 or not foreign_key2:
+            #     flash('Please be sure to select a primary key from both files')
+            #     return render_template('upload.html')
+
+            if not merge_headers1[0] or not merge_headers2[0]:
+                flash('Please be sure to select data to merge')
+                return render_template('upload.html')
         
+            no_zero = [] #make no_zero an empty list so that it doesn't enter if statement below
+
+
+
+
         ## TO DO remove hardcoding of Transacation Amt & $0
         # print("JUST BEFORE NOZERO IF", form)
         # if no_zero and no_zero[0] == 'on':
         #     print("NO_ZERO ",no_zero)
         #     return render_template('ignore_fields.html', merge_headers1=merge_headers1, merge_headers2=merge_headers2, filename1=filename1, filename2=filename2, form=form)
         #checking to see if ignore_field1 or ignore_field2 (or both) were checked. if not, sets equal to null
-        
-        if 'ignore_field1' in request.form.keys():    
-            ignore_field1 = request.form['ignore_field1'].replace("_"," ")
-            ignore_value1 = request.form['ignore_value1']
-
-        else:
-            ignore_field1 = None
-
-        if 'ignore_field2' in request.form.keys():    
-            ignore_field2 = request.form['ignore_field2'].replace("_"," ")
-            ignore_value2 = request.form['ignore_value2']
-        else: 
-            ignore_field2 = None
-  
-
-        #this will be the pandas code here
-        #open spreadsheets to begin merge process
+        print("MERGE_HEADERS 1 OUTSIDE IFS ", merge_headers1)
         df1 = pd.read_csv(path1)
         df2 = pd.read_csv(path2)
 
+        ignore_msg = ""
+
+        print("FOREIGN KEY1", foreign_key1)
+        print("FOREIGN KEY2", foreign_key2)
+
+        # if 'ignore_field1' in request.form.keys():  
+        #     ignore_field1 = request.form['ignore_field1'].replace("_"," ")
+        #     ignore_value1 = request.form['ignore_value1']
+        #     if ignore_value1 in df1[ignore_field1]:
+        #         print("GOT HERE")
+        #         print("MERGE_HEADERS 1 ", merge_headers1)
+        #         return render_template('ignore_fields.html', ignore_msg=ignore_msg,merge_headers1=merge_headers1, merge_headers2=merge_headers2, filename1=filename1, filename2=filename2, form=form)
+
+        # else:
+        #     ignore_field1 = None
+
+        # if 'ignore_field2' in request.form.keys(): 
+        #     ignore_field2 = request.form['ignore_field2'].replace("_"," ")
+        #     ignore_value2 = request.form['ignore_value2']
+        #     if ignore_value2 not in df2[ignore_field2].values:
+        #         print("GOT HERE")
+        #         print("MERGE_HEADERS 1 ", merge_headers1)
+        #         ignore_msg = "Value not found in column selected. Please try another value or column."
+        #         return render_template('ignore_fields.html', ignore_msg=ignore_msg,merge_headers1=merge_headers1, merge_headers2=merge_headers2, filename1=filename1, filename2=filename2, form=form)
+        # else: 
+        #     ignore_field2 = None
+
+        if 'ignore_field1' in request.form.keys():
+            if 'ignore_field1' not in df1.columns:
+                ignore_field1 = request.form['ignore_field1'].replace("_"," ")
+            ignore_value1 = request.form['ignore_value1']
+        else:
+            ignore_field1 = None
+                    #break
+                #except:
+                    #ignore_msg = "Value not found in column selected. Please try another value or column."
+                    #return render_template('ignore_fields.html', ignore_msg=ignore_msg,merge_headers1=merge_headers1, merge_headers2=merge_headers2, filename1=filename1, filename2=filename2, form=form)
+
+        print("HHHHHEEEEELLLLLLLLLOOOOOOOOOOOO")        
         
-        msg = ""
+        if 'ignore_field2' in request.form.keys():    
+            if 'ignore_field2' not in df2.columns:
+                ignore_field2 = request.form['ignore_field2'].replace("_"," ")
+            ignore_value2 = request.form['ignore_value2']
+        else:
+            ignore_field2 = None
+                    #break
+                #except:
+                    #ignore_msg = "Value not found in column selected. Please try another value or column."
+                    #print("EXCEPT FOR FLD2")
+                    #return render_template('ignore_fields.html', ignore_msg=ignore_msg,merge_headers1=merge_headers1, merge_headers2=merge_headers2, filename1=filename1, filename2=filename2, form=form)
+                    
+
+        #this will be the pandas code here
+        #open spreadsheets to begin merge process
+
+
+        
+        
         if ignore_field1:
             ignore1_count = len(df1[df1[ignore_field1] == ignore_value1])
-            msg = msg + "Excluded "+ignore_value1+" values from "+filename1+" "+str(ignore1_count)+" times \n"
+            ignore_msg = ignore_msg + "Excluded "+ignore_value1+" values from "+filename1+" "+str(ignore1_count)+" times \n"
             df1 = df1.loc[df1[ignore_field1] != ignore_value1]
+        else:
+            ignore1_count = None
+            ignore_value1 = None
         if ignore_field2:
             ignore2_count = len(df2[df2[ignore_field2] == ignore_value2])
-            msg = msg + "Excluded "+ignore_value2+" values from "+filename2+" "+str(ignore2_count)+" times \n" 
+            ignore_msg = ignore_msg + "Excluded "+ignore_value2+" values from "+filename2+" "+str(ignore2_count)+" times \n" 
             df2 = df2.loc[df2[ignore_field2] != ignore_value2]
+        else:
+            ignore2_count = None
+            ignore_value2 = None
 
-        for i in range(len(merge_headers1)): 
-            merge_headers1[i] = merge_headers1[i].replace("_"," ")
+        print("MERGE HEADERS 1 BEFORE APPEND", merge_headers1)    
+        merge_headers1.append(foreign_key1)
+        merge_headers2.append(foreign_key2)
+
+        #check to see if column names' syntax match what is in natural state of files.
+        #if not, add spaces where there are underscores
+        for i in range(len(merge_headers1)):
+            if merge_headers1[i] not in df1.columns:
+                merge_headers1[i] = merge_headers1[i].replace("_"," ")
         for i in range(len(merge_headers2)):
-            merge_headers2[i] = merge_headers2[i].replace("_"," ")        
-        foreign_key1 = foreign_key1.replace("_"," ")
-        foreign_key2 = foreign_key2.replace("_"," ")
+            if merge_headers2[i] not in df2.columns:
+                merge_headers2[i] = merge_headers2[i].replace("_"," ")
+        if foreign_key1 not in df1.columns:             
+            foreign_key1 = foreign_key1.replace("_"," ")
+        if foreign_key2 not in df2.columns:
+            foreign_key2 = foreign_key2.replace("_"," ")
 
         
         #THIS IS NEW CODE ADDED ON SUNDAY 4/26
-        df2.rename(columns = {foreign_key2:foreign_key1}, inplace = True)
-        merge_headers1.append(foreign_key1)
-        merge_headers2.append(foreign_key1)
+        #df2.rename(columns = {foreign_key2:foreign_key1}, inplace = True)
+
+        print("MERGE HEADERS 1",merge_headers1)
+
         df1 = df1[merge_headers1]
         df2 = df2[merge_headers2]
+
+        print("foreign_key1", foreign_key1)
+        print("foreign_key2", foreign_key2)
+        print(df1.columns)
+        print(df2.columns)
 
         #get percentage of merge matches
         matches = set(df1[foreign_key1]).intersection(set(df2[foreign_key2]))
         percent = len(matches) / len(df1) * 100.00
+        print('PERCENT ',percent)
 
-        if percent <= 5.00:
-            return render_template('upload.html',msg="Foreign Key Match was less than 5% \n")
+        if percent <= 0:
+            flash('Foreign Key Match was less than 1%, is there a better join field?')
+            msg="Foreign Key Match was less than 5%"
+            return render_template('upload.html')
         else:
             print("Files Merged W/ ", percent)
-            msg = msg + str(round(percent,2))+"% of rows in file 1 matched file 2 \n"
+            msg = str(round(percent,2))+"% of rows in File 1 matched File 2"
             #msg2 = "Excluded values "+ignore_value2
 
 
             save_file=filename1[:-4]+"-"+filename2[:-4]+".csv"
             file_src=UPLOAD_FOLDER + "/" + save_file      
-            merged_file = pd.merge(df1, df2, on = foreign_key1, how='left')            
+            #merged_file = pd.merge(df1, df2, on = foreign_key1, how='left') 
+            merged_file = pd.merge(df1, df2, left_on = foreign_key1, right_on = foreign_key2, how='left')           
             merged_file.to_csv(file_src)
 
-        return render_template('merged_file.html', msg=msg, merge_headers1=merge_headers1, merge_headers2=merge_headers2, foreign_key1=foreign_key1, foreign_key2=foreign_key2, form=form, no_zero=no_zero, UPLOAD_FOLDER=UPLOAD_FOLDER, save_file=save_file, filename1=filename1, filename2=filename2) 
+        return render_template('merged_file.html', msg=msg,
+            ignore_msg=ignore_msg, ignore_value1=ignore_value1,
+            ignore_value2=ignore_value2, ignore1_count=ignore1_count,
+            ignore2_count=ignore2_count, merge_headers1=merge_headers1,
+            merge_headers2=merge_headers2, foreign_key1=foreign_key1,
+            foreign_key2=foreign_key2, form=form, no_zero=no_zero,
+            UPLOAD_FOLDER=UPLOAD_FOLDER, save_file=save_file,
+            filename1=filename1, filename2=filename2) 
     elif request.method == 'GET':
         return render_template('upload.html')        
 
